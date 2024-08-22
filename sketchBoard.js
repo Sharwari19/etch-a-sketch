2a5925c6f0f8031542967bb026952d3f5e4a797a
sharwarigole <sharwarigole@gmail.com>
2024-08-22 17:36:23 +0530

    feat: function to generate hex color from user input

--- sketchBoard.js
+++ sketchBoard.js
@@ -3,7 +3,6 @@ const gridContainer = document.querySelector('.grid-container');
 const button = document.createElement("button");
 const colorButton = document.createElement("button");
 const buttonDiv = document.createElement("div");
-const DARK_GREEN_COLOR = '#006A4E';
 const NEON_GREEN_COLOR = '#ACE1AF';
 
 buttonDiv.setAttribute("class", "btn-container");
@@ -18,46 +17,53 @@ button.setAttribute("id", "size-btn");
 buttonDiv.appendChild(colorButton);
 
 
-for(let i = 1; i <= 256; i++)
+function gridDivsCreation(color = NEON_GREEN_COLOR)
 {
-    let hoverCount = 0;
-    let currenOpacity = 0.1;
-    let maxOpacity = 1;
-    const increment = 0.1;
-    // let currentBrightness = 1;
-
-    const gridDiv = document.createElement("div");
-    gridDiv.setAttribute("class", "grid-item");
-    gridContainer.appendChild(gridDiv);
-
-    gridDiv.addEventListener('mouseover', () => {
-
-        gridDiv.style.backgroundColor = NEON_GREEN_COLOR;
-        hoverCount++;
-        console.log('hoverCount: ', hoverCount);
-
-        // currentBrightness = currentBrightness - decrement;
-        // if(currentBrightness < 0.1)
-        // {
-        //     currentBrightness = 0.1;
-        // }
-        // gridDiv.style.filter = `brightness(${currentBrightness})`;
-
-        currenOpacity = currenOpacity + increment;
-        if(currenOpacity  > maxOpacity)
+    gridContainer.innerHTML = '';
+    gridContainer.style.width = '500px';
+    gridContainer.style.height = '500px';
+    
+    for(let i = 1; i <= 256; i++)
         {
-            currenOpacity = maxOpacity;
+            let hoverCount = 0;
+            let currenOpacity = 0.1;
+            let maxOpacity = 1;
+            const increment = 0.1;
+            // let currentBrightness = 1;
+        
+            const gridDiv = document.createElement("div");
+            gridDiv.setAttribute("class", "grid-item");
+            gridContainer.appendChild(gridDiv);
+        
+            gridDiv.addEventListener('mouseover', () => {
+        
+                gridDiv.style.backgroundColor = color;
+                hoverCount++;
+                console.log('hoverCount: ', hoverCount);
+        
+                // currentBrightness = currentBrightness - decrement;
+                // if(currentBrightness < 0.1)
+                // {
+                //     currentBrightness = 0.1;
+                // }
+                // gridDiv.style.filter = `brightness(${currentBrightness})`;
+        
+                currenOpacity = currenOpacity + increment;
+                if(currenOpacity  > maxOpacity)
+                {
+                    currenOpacity = maxOpacity;
+                }
+        
+                gridDiv.style.opacity = `${currenOpacity}`
+        
+                if(hoverCount > 9)
+                {
+                    gridDiv.style.backgroundColor = color;
+                    hoverCount = 0;
+                }
+            })
+        
         }
-
-        gridDiv.style.opacity = `${currenOpacity}`
-
-        if(hoverCount > 9)
-        {
-            gridDiv.style.backgroundColor = NEON_GREEN_COLOR;
-            hoverCount = 0;
-        }
-    })
-
 }
 
 let userInput = '';
@@ -128,5 +134,29 @@ function handleCustomizing()
 
 button.addEventListener('click', handleCustomizing);
 
+function colorToHex(userColor)
+{
+    const colors = {
+        black: "#000000",
+        red: "#FF0000",
+        lime: "#00FF00",
+        maroon: "#800000",
+        teal: "#008080",
+    };
+
+    return colors[userColor.toLowerCase()] || "Invalid color name";
+
+}
+
+function handleColorChange()
+{
+    const userSelColor = prompt("Choose a color between : black, red, lime, maroon or teal");
+    const hexValue = colorToHex(userSelColor);
+    gridDivsCreation(hexValue); 
+
+}
+
+colorButton.addEventListener('click',  handleColorChange);
 
 
+gridDivsCreation();
