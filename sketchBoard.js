ab8955d0f90e2049673e6f69fe0bba5dfe2db4b4
sharwarigole <sharwarigole@gmail.com>
2024-08-22 12:32:08 +0530

    create a grid container sizing full screen

---
+++ sketchBoard.js
@@ -0,0 +1,8 @@
+const gridContainer = document.querySelector('.grid-container');
+
+const gridDiv = document.createElement("div");
+gridDiv.setAttribute("class", "grid-item");
+// gridDiv.style.width = '16px';
+// gridDiv.style.height = '16px';
+
+gridContainer.appendChild(gridDiv);
