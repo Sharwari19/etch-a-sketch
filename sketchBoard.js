e28071e16bb0ded45ab844379a81bb22d5773bc0
sharwarigole <sharwarigole@gmail.com>
2024-08-22 14:43:48 +0530

    feat: Add button to dynamically resize grid based on user input with max 100 squares per side

--- sketchBoard.js
+++ sketchBoard.js
@@ -1,4 +1,6 @@
+const body = document.querySelector("body");
 const gridContainer = document.querySelector('.grid-container');
+const button = document.createElement("button");
 
 for(let i = 1; i <= 256; i++)
 {
@@ -13,4 +15,46 @@ for(let i = 1; i <= 256; i++)
         gridDiv.style.backgroundColor = '#ACE1AF';
     })
 }
+
+body.appendChild(button);
+button.textContent = 'Click For Customizing'
+let userInput = '';
+let containerWidth = 500;
+let containerHeight = 500;
+
+function handleCustomizing()
+{
+    userInput = prompt("Enter the number of squares you want per side for the new grid");
+    if(userInput > 100)
+    {
+        userInput = prompt("Number of squares should be less than 100");
+    }
+    console.log('userInput: ', userInput);
+
+    gridContainer.innerHTML = '';
+    gridContainer.style.width = '500px';
+    gridContainer.style.height = '500px';
+    
+    for(let i = 1; i <= userInput*userInput; i++)
+    {
+        const newGridDiv = document.createElement("div");
+        newGridDiv.setAttribute("class", "new-item");
+        newGridDiv.style.width = `${containerWidth/userInput}px`;
+        newGridDiv.style.height = `${containerWidth/userInput}px`;
+        gridContainer.appendChild(newGridDiv);
+        console.log('box-width-height ', containerWidth/userInput);
+        
+
+        newGridDiv.addEventListener('mouseover', () => {
+            newGridDiv.style.backgroundColor = '#006A4E';
+        })
+    
+        newGridDiv.addEventListener('mouseout', () => {
+            newGridDiv.style.backgroundColor = '#ACE1AF';
+        })
+    }
+}
+
+button.addEventListener('click', handleCustomizing);
+
 
