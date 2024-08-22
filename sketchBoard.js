7620ce086d62dd5e5b851eb777d26eb7fcbfa9b8
sharwarigole <sharwarigole@gmail.com>
2024-08-22 16:12:50 +0530

    feat: Implement progressive darkening effect with 10% increment per interaction, achieving a dark square in 10 interactions

--- sketchBoard.js
+++ sketchBoard.js
@@ -1,19 +1,49 @@
 const body = document.querySelector("body");
 const gridContainer = document.querySelector('.grid-container');
 const button = document.createElement("button");
+const DARK_GREEN_COLOR = '#006A4E';
+const NEON_GREEN_COLOR = '#ACE1AF';
 
 for(let i = 1; i <= 256; i++)
 {
+    let hoverCount = 0;
+    let currenOpacity = 0.1;
+    let maxOpacity = 1;
+    const increment = 0.1;
+    // let currentBrightness = 1;
+
     const gridDiv = document.createElement("div");
     gridDiv.setAttribute("class", "grid-item");
     gridContainer.appendChild(gridDiv);
+
     gridDiv.addEventListener('mouseover', () => {
-        gridDiv.style.backgroundColor = '#006A4E';
+
+        gridDiv.style.backgroundColor = NEON_GREEN_COLOR;
+        hoverCount++;
+        console.log('hoverCount: ', hoverCount);
+
+        // currentBrightness = currentBrightness - decrement;
+        // if(currentBrightness < 0.1)
+        // {
+        //     currentBrightness = 0.1;
+        // }
+        // gridDiv.style.filter = `brightness(${currentBrightness})`;
+
+        currenOpacity = currenOpacity + increment;
+        if(currenOpacity  > maxOpacity)
+        {
+            currenOpacity = maxOpacity;
+        }
+
+        gridDiv.style.opacity = `${currenOpacity}`
+
+        if(hoverCount > 9)
+        {
+            gridDiv.style.backgroundColor = NEON_GREEN_COLOR;
+            hoverCount = 0;
+        }
     })
 
-    gridDiv.addEventListener('mouseout', () => {
-        gridDiv.style.backgroundColor = '#ACE1AF';
-    })
 }
 
 body.appendChild(button);
@@ -21,6 +51,7 @@ button.textContent = 'Click For Customizing'
 let userInput = '';
 let containerWidth = 500;
 let containerHeight = 500;
+
 
 function handleCustomizing()
 {
@@ -37,6 +68,12 @@ function handleCustomizing()
     
     for(let i = 1; i <= userInput*userInput; i++)
     {
+        let hoverCount = 0;
+        let currenOpacity = 0.1;
+        let maxOpacity = 1;
+        const increment = 0.1;
+        // let currentBrightness = 1;
+
         const newGridDiv = document.createElement("div");
         newGridDiv.setAttribute("class", "new-item");
         newGridDiv.style.width = `${containerWidth/userInput}px`;
@@ -46,13 +83,34 @@ function handleCustomizing()
         
 
         newGridDiv.addEventListener('mouseover', () => {
-            newGridDiv.style.backgroundColor = '#006A4E';
+            newGridDiv.style.backgroundColor = NEON_GREEN_COLOR;
+            hoverCount++;
+            console.log('hoverCount: ', hoverCount);
+
+            // currentBrightness = currentBrightness - decrement;
+            // if(currentBrightness < 0.1)
+            // {
+            //     currentBrightness = 0.1;
+            // }
+            // newGridDiv.style.filter = `brightness(${currentBrightness})`;
+
+            currenOpacity = currenOpacity + increment;
+            if(currenOpacity  > maxOpacity)
+            {
+                currenOpacity = maxOpacity;
+            }
+
+            newGridDiv.style.opacity = `${currenOpacity}`
+
+            if(hoverCount > 9)
+            {
+                newGridDiv.style.backgroundColor = NEON_GREEN_COLOR;
+                hoverCount = 0;
+            }
         })
     
-        newGridDiv.addEventListener('mouseout', () => {
-            newGridDiv.style.backgroundColor = '#ACE1AF';
-        })
     }
+
 }
 
 button.addEventListener('click', handleCustomizing);
